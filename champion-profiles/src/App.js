1593fff49367965b09f2232ae968f3d02c2d8279
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-08-31 11:37:48 +0530

    react

---
+++ champion-profiles/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import AthleteProfile from './Pages/AthleteProfile';
+
+const App = () => {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/athlete-profile/:name" element={<AthleteProfile />} />
+      </Routes>
+  );
+};
+
+export default App;
